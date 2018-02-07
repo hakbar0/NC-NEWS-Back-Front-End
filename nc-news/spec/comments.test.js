c56b1ccb581ab170eda00e4b4ecf94c70d6cb285
Haseeb Akbar <haseeb2000@hotmail.com>
2018-02-07 21:42:46 +0000

    Able to test stories and comments

--- nc-news/spec/comments.test.js
+++ nc-news/spec/comments.test.js
@@ -1,30 +1,6 @@
-// tests for all comments features!!
+// tests for all comment features.
 
 db = require('./firebase/firebaseTest');
-
-it('updates vote count in comment for an upvote.', () => {
-  return db.ref(`/Stories/-L4erJ5eJo56Bvs5BsjK`).once("value", res => {
-  }).then(number => {
-    previousVote = number.val().votes;
-    return db.ref(`/Stories/-L4erJ5eJo56Bvs5BsjK`).update({ votes: previousVote + 1 })
-  }).then(somthing => {
-    return db.ref(`/Stories/-L4erJ5eJo56Bvs5BsjK`).once("value", currentVote => {
-      expect(previousVote + 1).toBe(currentVote.val().votes);
-    })
-  })
-})
-
-it('updates vote count in comment for a downvote.', () => {
-  return db.ref(`/Stories/-L4erJ5eJo56Bvs5BsjK`).once("value", res => {
-  }).then(number => {
-    previousVote = number.val().votes;
-    return db.ref(`/Stories/-L4erJ5eJo56Bvs5BsjK`).update({ votes: previousVote - 1 })
-  }).then(somthing => {
-    return db.ref(`/Stories/-L4erJ5eJo56Bvs5BsjK`).once("value", currentVote => {
-      expect(previousVote - 1).toBe(currentVote.val().votes);
-    })
-  })
-})
 
 it('Able to post a comment.', () => {
   return db.ref("/Comments").once('value', commentsCount => {
