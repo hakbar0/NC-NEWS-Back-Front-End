05f27daf8b8d89bac2f4a295d4e8b64af408474e
Haseeb Akbar <haseeb2000@hotmail.com>
2018-02-07 13:04:57 +0000

    able to delete only northcoders comments

--- nc-news/src/components/article.js
+++ nc-news/src/components/article.js
@@ -109,7 +109,10 @@ class Article extends React.Component {
   }
 
   deleteComment = (id) => {
-    db.ref(`/Comments/${id}`).remove();
+    db.ref(`/Comments/${id}`).once('value', res => {
+      if(res.val().fullname === 'northcoder') db.ref(`/Comments/${id}`).remove();
+      else if (res.val().fullname !== 'northcoder') alert('You cannot delete this comment!!');
+    })
   }
 }
 export default Article;
